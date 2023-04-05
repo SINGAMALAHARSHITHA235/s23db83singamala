4ef119d8ab484ec2f84aaf794dd908c5ff546e35
Harshitha Singamala <https://github.com/SINGAMALAHARSHITHA235/s23wa83singamala.git>
2023-04-05 14:40:27 -0500

    added grid layout files

--- routes/cameras.js
+++ routes/cameras.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Harshitha' });
+  res.render('cameras', { title: 'Search Results cameras' });
 });
 
 module.exports = router;
