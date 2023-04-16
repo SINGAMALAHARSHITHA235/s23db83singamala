a0d4c2b247e83d0e6beda6dca65b789ffc5fe48a
Harshitha Singamala <https://github.com/SINGAMALAHARSHITHA235/s23wa83singamala.git>
2023-04-16 11:43:43 -0500

    added the new file

--- controllers/cameras.js
+++ controllers/cameras.js
@@ -29,13 +29,38 @@ exports.cameras_create_post = async function(req, res) {
     } 
 };
 // Handle cameras delete form on DELETE.
-exports.cameras_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: cameras delete DELETE ' + req.params.id);
-};
+exports.cameras_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await cameras.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+    
 // Handle cameras update form on PUT.
-exports.cameras_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: cameras update PUT' + req.params.id);
-};
+exports.cameras_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await cameras.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.cameras_type)  
+               toUpdate.cameras_type = req.body.cameras_type; 
+        if(req.body.cameras_resolution) toUpdate.cameras_resolution = req.body.cameras_resolution; 
+        if(req.body.cameras_price) toUpdate.cameras_price = req.body.cameras_price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 // VIEWS
 // Handle a show all view
 exports.cameras_view_all_Page = async function(req, res) {
